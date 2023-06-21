5ae7c5e6b0d4919ab7b6f7f97d23d27180f874ab
unknown <ama-ali@hotmail.co.uk>
2023-06-21 20:46:35 +0100

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var numbers = "0123456789";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+//function to generate password
+
+function generatePassword () {
+  var passwordLength = parseInt
+  prompt("Enter the length of the password(8-128 characters):")
+};
+
+console.log("password length:", passwordLength);
+
+//validating length of password
+
+if ( 
+  typeof passwordLength !== "number" ||
+  passwordLength < 8 ||
+  passwordLength > 128
+
+){
+  alert("invalid password length. Please enter number betwwen 8-128.");
+  return "";
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
